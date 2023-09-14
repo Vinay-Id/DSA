c325f3361f1902adc53cb1f2cf53ac6d81fcb493
Vinay Vishwakarma <vnypro98@gmail.com>
2023-09-14 19:30:32 +0530

    added few more programs

---
+++ algorithm/sortingAlgoritm.js
@@ -0,0 +1,13 @@
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+console.log(bubbleSort([25,6,78,9]))
