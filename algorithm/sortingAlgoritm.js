f627f60c98e3c73cd22568bcff379e8f3c154def
Vinay Vishwakarma <vnypro98@gmail.com>
2023-09-15 19:58:33 +0530

    added few more sorting alogirthm

--- algorithm/sortingAlgoritm.js
+++ algorithm/sortingAlgoritm.js
@@ -1,13 +1,37 @@
-function bubbleSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
+// function bubbleSort(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+//   return arr;
+// }
+// console.log(bubbleSort([25, 6, 78, 9]));
+
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const pivot = arr[0];
+  const left = [];
+  const right = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
     }
   }
-  return arr;
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
 }
-console.log(bubbleSort([25,6,78,9]))
+
+const arr = [64, 34, 25, 12, 22, 11, 90];
+const sortedArr = quickSort(arr);
+console.log(sortedArr); // Output: [11, 12, 22, 25, 34, 64, 90]
