92499e60770b6b0f9fc3d6544a3ffb5b289b3b68
Vinay Vishwakarma <vnypro98@gmail.com>
2023-09-21 22:34:32 +0530

    added few more programs

--- DSA/removingDuplicateArray.js
+++ DSA/removingDuplicateArray.js
@@ -1,17 +1,30 @@
-const arr1 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+// const arr1 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
 
+// const removeDup = [];
+
+// for (let i = 0; i < arr1.length; i++) {
+//   if (i < arr1.length - 1) {
+//     if (arr1[i] != arr1[i + 1]) {
+//       removeDup.push(arr1[i]);
+//     }
+//   } else {
+//     removeDup.push(arr1[i]);
+//   }
+// }
+
+// console.log(arr1);
+
+// console.log(removeDup);
+
+
+const arr1 = [0,0,1,1,1,2,2,3,3,4];
 const removeDup = [];
 
-for (let i = 0; i < arr1.length; i++) {
-  if (i < arr1.length - 1) {
-    if (arr1[i] != arr1[i + 1]) {
-      removeDup.push(arr1[i]);
-    }
-  } else {
-    removeDup.push(arr1[i]);
-  }
+for (let i = 0; i < arr1.length; i++){
+   if(!removeDup.includes(arr1[i])){
+    removeDup.push(arr1[i])
+   }
 }
 
 console.log(arr1);
-
 console.log(removeDup);
