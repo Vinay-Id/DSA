bc25898690472424084d1012de4252998b3e49bc
Vinay Vishwakarma <vnypro98@gmail.com>
2023-11-17 19:44:08 +0530

    more programs added

---
+++ basic programs/createTargetArrayInTheGivenOrder.js
@@ -0,0 +1,28 @@
+// Example 1:
+// Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+// Output: [0,4,1,3,2]
+// Explanation:
+// nums       index     target
+// 0            0        [0]
+// 1            1        [0,1]
+// 2            2        [0,1,2]
+// 3            2        [0,1,3,2]
+// 4            1        [0,4,1,3,2]
+// Example 2:
+// Input: nums = [1,2,3,4,0], index = [0,1,2,3,0]
+// Output: [0,1,2,3,4]
+// Explanation:
+// nums       index     target
+// 1            0        [1]
+// 2            1        [1,2]
+// 3            2        [1,2,3]
+// 4            3        [1,2,3,4]
+// 0            0        [0,1,2,3,4]
+
+var createTargetArray = function (nums, index) {
+  let arr = [];
+  for (let i = 0; i < index.length; i++) {
+    arr.splice(index[i], 0, nums[i]);
+  }
+  return arr;
+};
