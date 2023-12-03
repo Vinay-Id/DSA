99b0048789c3ca5afca2457e2fb3bbd34217539c
Vinay Vishwakarma <vnypro98@gmail.com>
2023-12-03 21:21:50 +0530

    few more programs added

---
+++ basic programs/noOfStringsThatAppearAsSubstringsInWord.js
@@ -0,0 +1,28 @@
+// Example 1:
+// Input: patterns = ["a","abc","bc","d"], word = "abc"
+// Output: 3
+// Explanation:
+// - "a" appears as a substring in "abc".
+// - "abc" appears as a substring in "abc".
+// - "bc" appears as a substring in "abc".
+// - "d" does not appear as a substring in "abc".
+// 3 of the strings in patterns appear as a substring in word.
+// Example 2:
+// Input: patterns = ["a","b","c"], word = "aaaaabbbbb"
+// Output: 2
+// Explanation:
+// - "a" appears as a substring in "aaaaabbbbb".
+// - "b" appears as a substring in "aaaaabbbbb".
+// - "c" does not appear as a substring in "aaaaabbbbb".
+// 2 of the strings in patterns appear as a substring in word.
+
+
+var numOfStrings = function (patterns, word) {
+  let count = 0;
+  for (let i = 0; i < patterns.length; i++) {
+    if (word.includes(patterns[i]) == true) {
+      count++;
+    }
+  }
+  return count;
+};
