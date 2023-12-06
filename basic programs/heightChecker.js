d6f74582778ceff343aa0b91c759450eefad8e40
Vinay Vishwakarma <vnypro98@gmail.com>
2023-12-06 10:36:31 +0530

    few more programs added

---
+++ basic programs/heightChecker.js
@@ -0,0 +1,32 @@
+// Example 1:
+// Input: heights = [1,1,4,2,1,3]
+// Output: 3
+// Explanation: 
+// heights:  [1,1,4,2,1,3]
+// expected: [1,1,1,2,3,4]
+// Indices 2, 4, and 5 do not match.
+// Example 2:
+// Input: heights = [5,1,2,3,4]
+// Output: 5
+// Explanation:
+// heights:  [5,1,2,3,4]
+// expected: [1,2,3,4,5]
+// All indices do not match.
+// Example 3:
+// Input: heights = [1,2,3,4,5]
+// Output: 0
+// Explanation:
+// heights:  [1,2,3,4,5]
+// expected: [1,2,3,4,5]
+// All indices match.
+
+var heightChecker = function(heights) {
+    let expected=[...heights].sort((a,b)=>a-b);
+    let result=0;
+    for(let i=0;i<heights.length;i++){
+        if(heights[i]!==expected[i]){
+            result++;
+        }
+    }
+    return result;
+};
