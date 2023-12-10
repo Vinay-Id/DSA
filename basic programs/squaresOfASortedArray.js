01ec448262c26d45242b0ef39a21433cb49c362e
Vinay Vishwakarma <vnypro98@gmail.com>
2023-12-10 09:37:42 +0530

    few more programs added

---
+++ basic programs/squaresOfASortedArray.js
@@ -0,0 +1,12 @@
+// Example 1:
+// Input: nums = [-4,-1,0,3,10]
+// Output: [0,1,9,16,100]
+// Explanation: After squaring, the array becomes [16,1,0,9,100].
+// After sorting, it becomes [0,1,9,16,100].
+// Example 2:
+// Input: nums = [-7,-3,2,3,11]
+// Output: [4,9,9,49,121]
+
+var sortedSquares = function (nums) {
+  return nums.map((elem) => elem ** 2).sort((a, b) => a - b);
+};
