88c7f0c13b92a32ff4b77c76a349753dd24212c6
Vinay Vishwakarma <vnypro98@gmail.com>
2023-12-17 20:18:06 +0530

    few more programs added

---
+++ basic programs/mergeStringsAlternately.js
@@ -0,0 +1,27 @@
+// Example 1:
+// Input: word1 = "abc", word2 = "pqr"
+// Output: "apbqcr"
+// Explanation: The merged string will be merged as so:
+// word1:  a   b   c
+// word2:    p   q   r
+// merged: a p b q c r
+// Example 2:
+// Input: word1 = "ab", word2 = "pqrs"
+// Output: "apbqrs"
+// Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+// word1:  a   b
+// word2:    p   q   r   s
+// merged: a p b q   r   s
+var mergeAlternately = function (word1, word2) {
+  let result = "";
+  const maxLength = Math.max(word1.length, word2.length);
+  for (let i = 0; i < maxLength; i++) {
+    if (i < word1.length) {
+      result += word1[i];
+    }
+    if (i < word2.length) {
+      result += word2[i];
+    }
+  }
+  return result;
+};
