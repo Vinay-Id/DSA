60b2dac1d500fd15e8d271f714987b54be8fee46
Vinay Vishwakarma <vnypro98@gmail.com>
2023-12-30 22:09:14 +0530

    few more programs added

---
+++ basic programs/redistributeCharactersToMakeAllStringsEqual.js
@@ -0,0 +1,18 @@
+var makeEqual = function (words) {
+  const charCount = new Map();
+
+  for (const word of words) {
+    for (const char of word) {
+      charCount.set(char, (charCount.get(char) || 0) + 1);
+    }
+  }
+
+  const totalWords = words.length;
+  for (const count of charCount.values()) {
+    if (count % totalWords !== 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
