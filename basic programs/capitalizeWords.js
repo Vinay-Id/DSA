1c4609590965f7eb8b9c508ed4b87f93db081a28
vinay vishwakarma <vnypro98@gmail.com>
2024-07-08 07:18:17 +0100

    added few code

---
+++ basic programs/capitalizeWords.js
@@ -0,0 +1,8 @@
+function capitalizeWords(sentence) {
+  return sentence
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
+console.log(capitalizeWords("hello world"));
