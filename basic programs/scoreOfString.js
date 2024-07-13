64f492ba06c177cc55eb0fcdb9be9ea0be13bf8d
vinay vishwakarma <vnypro98@gmail.com>
2024-07-13 17:00:04 +0100

    one more program added

--- basic programs/scoreOfString.js
+++ basic programs/scoreOfString.js
@@ -1,3 +1,16 @@
+// Example 1:
+// Input: s = "hello"
+// Output: 13
+// Explanation:
+// The ASCII values of the characters in s are: 'h' = 104, 'e' = 101, 'l' = 108, 'o' = 111. So, the score of s would be |104 - 101| + |101 - 108| + |108 - 108| + |108 - 111| = 3 + 7 + 0 + 3 = 13.
+
+// Example 2:
+// Input: s = "zaz"
+// Output: 50
+// Explanation:
+// The ASCII values of the characters in s are: 'z' = 122, 'a' = 97. So, the score of s would be |122 - 97| + |97 - 122| = 25 + 25 = 50.
+
+
 const scoreOfString = function(s) {
     let finalVal = 0;
     
